c4ad93e55ae4173224df54947a8eecba3b71a95a
Jozo Slejko <jozo.slejko@gmail.com>
2022-01-02 02:22:16 +0000

    Change web tests

--- tests/Website.Tests.ps1
+++ tests/Website.Tests.ps1
@@ -13,12 +13,12 @@ param(
 Describe 'Check Ghost Website' {
 
     It 'Serves pages' {
-      $request = [System.Net.WebRequest]::Create("https://$HostName/")
-      $request.AllowAutoRedirect = $true
-      $request.GetResponse()
-      $request.GetResponse()
-      Start-Sleep -Seconds 300
-      $request.GetResponse().StatusCode |
+      for ($i = 0; $i -lt 4; $i++) {
+        Invoke-WebRequest "https://$HostName/"
+        Start-Sleep -Seconds 30
+      }
+      $request = Invoke-WebRequest "https://$HostName/"
+      $request.StatusCode |
         Should -Be 200 -Because "the website works"
     }
 
@@ -26,17 +26,16 @@ Describe 'Check Ghost Website' {
 
 Describe 'Check Ghost Website Slot' {
 
-  It 'Serves pages' -Skip:($SlotHostName -eq $null)  {
-      $request = [System.Net.WebRequest]::Create("https://$SlotHostName/")
-      $request.AllowAutoRedirect = $true
-      $request.GetResponse()
-      $request.GetResponse()
-      Start-Sleep -Seconds 300
-      $request.GetResponse().StatusCode |
+  It 'Serves pages' -Skip:($SlotHostName) {
+      for ($i = 0; $i -lt 4; $i++) {
+        Invoke-WebRequest "https://$SlotHostName/"
+        Start-Sleep -Seconds 30
+      }
+      $request = Invoke-WebRequest "https://$SlotHostName/"
+      $request.StatusCode |
         Should -Be 200 -Because "the website works"
-  }
+    }
 }
-
 # Describe "Check Ghost Function App" {
   
 #   It "Redirects to AAD login" {
